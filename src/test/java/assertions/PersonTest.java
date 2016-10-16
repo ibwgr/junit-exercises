9627215e0e77d624fba2fe88ec0536a80848e244
Harry Moser <harry.moser@hispeed.ch>
2016-10-16 08:38:21 +0200

    Test PersonTest erweitert.

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -1,9 +1,11 @@
 package assertions;
 
+import junit.framework.Assert;
 import org.junit.Test;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * Created by ideadapt on 09.10.16.
@@ -14,21 +16,37 @@ public class PersonTest {
 
     @Test
     public void getFullNameReturnsFirstnameSpaceLastname(){
-        // TODO implement
-        throw new NotImplementedException();
+        Person p = new Person("Harry", "Moser", LocalDate.now());
+
+        String fullname = p.getFullName();
+
+        Assert.assertEquals("Harry Moser", fullname);
     }
 
     // TODO some more useful tests
+    @Test
+    public void getAgeReturnTestObObjektPeriodeZurckKommt(){
+        Person t = new Person("Harry", "Moser", LocalDate.now());
+        Period  period = t.getAge();
+        Assert.assertNotNull(period);
 
-    // getAge
+    }
+    @Test
+    public void getAgeRetunObAlterRichtigRechnet() {
+        Person p = new Person("Harry", "Moser", LocalDate.of(1973, 04, 10));
+        Period  period = p.getAge();
+        Assert.assertEquals(43,period.getYears());
 
+
+
+    }
 
     // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
     @Test
     public void getAgeReturns10YearsIf2006() throws Exception {
         Person p = new Person("", "", LocalDate.of(2006, 1, 1));
 
-        throw new NotImplementedException();
+
     }
 
     @Test
