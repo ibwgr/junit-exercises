465d1d4cbe5687dc96aab1fa95534840f268e19d
Megamage <radee92@gmail.com>
2019-10-25 16:36:50 +0200

    V1.2
    More changes

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -2,9 +2,9 @@ package assertions;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 class PersonTest {
 
@@ -13,7 +13,7 @@ class PersonTest {
     @Test
     void getFullNameReturnsFirstnameSpaceLastname(){
         // TODO implement
-        Person p = new Person("Rade", "Ilic", -03-1992);
+        Person p = new Person("Rade", "Ilic", LocalDate.of(1992, 3, 9));
 
         String fullName = p.getFullName();
 
@@ -28,17 +28,24 @@ class PersonTest {
     // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
     @Test
     void getAgeReturns10YearsIfBornIn2009() throws Exception {
-        Person p = new Person("", "", LocalDate.of(2009, 1, 1));
+        Person p = new Person("", "", LocalDate.now().minusYears(10));
 
-        throw new NotImplementedException();
+        Period age = p.getAge();
+
+        Assertions.assertEquals(10, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(0, age.getDays());
     }
 
     @Test
     void getAgeReturns1DayIfYesterday() throws Exception {
-        Person p = new Person("", "", LocalDate.now().minusDays(1));
+         Person p = new Person("", "", LocalDate.now().minusDays(1));
 
-        // TODO implement
-        throw new NotImplementedException();
+        Period age = p.getAge();
+
+        Assertions.assertEquals(0, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(1, age.getDays());
     }
     // TODO some more useful tests
 }
--- src/test/java/fakes/UserValidatorTest.java
+++ src/test/java/fakes/UserValidatorTest.java
@@ -2,7 +2,6 @@ package fakes;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 class UserValidatorTest {
 
@@ -11,22 +10,20 @@ class UserValidatorTest {
 
         @Test
         void returnsTrueIfOnlyLetters(){
-            throw new NotImplementedException();
+
         }
 
         @Test
         void returnsFalseIfStartsWithNumber(){
-            throw new NotImplementedException();
         }
 
         @Test
         void returnsTrueIfContainsNumberButNotAsFirstChar(){
-            throw new NotImplementedException();
+
         }
 
         @Test
         void returnsFalseIfContainsAnyNonAlphanumericChar(){
-            throw new NotImplementedException();
         }
     }
 
@@ -34,12 +31,12 @@ class UserValidatorTest {
 
         @Test
         void returnsTrueIfUsernameNotInDBYet(){
-            throw new NotImplementedException();
+
         }
 
         @Test
         void returnsFalseIfUsernameInDB(){
-            throw new NotImplementedException();
+
         }
     }
 }
