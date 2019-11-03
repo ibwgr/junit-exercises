06a5453db943f15af2e4b56dfe9598881111aeb1
ptuor <tuor_philipp@hotmail.com>
2019-11-03 10:28:40 +0100

    my unit test suggestion

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -1,39 +1,84 @@
 package assertions;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 class PersonTest {
 
-    // getFullName
-
+    /***********************
+    * getFullName
+    */
     @Test
-    void getFullNameReturnsFirstnameSpaceLastname(){
-        // TODO implement
-        throw new NotImplementedException();
+    void getFullNameShouldReturnFirstnameSpaceLastname(){
+        Person a = new Person("Philipp","Tuor", LocalDate.of(1989, 06, 21));
+
+        String fullName = a.getFullName();
+
+        Assert.assertEquals("Philipp Tuor", fullName);
     }
 
-    // TODO some more useful tests
-
-    // getAge
-
-
-    // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
     @Test
-    void getAgeReturns10YearsIfBornIn2009() throws Exception {
-        Person p = new Person("", "", LocalDate.of(2009, 1, 1));
+    void getFullNameShouldReturnFirstnameSpaceDoubleLastname() {
+        Person a = new Person(" Gülän", "DoubleLastName 123", LocalDate.of(1989, 06, 21));
 
-        throw new NotImplementedException();
+        String fullName = a.getFullName();
+
+        Assert.assertEquals(" Gülän DoubleLastName 123", fullName);
+    }
+
+    @Test
+    void getFullNameShouldNotReturnFirstnameLastname(){
+        Person a = new Person("Philipp","Tuor", LocalDate.of(1989, 06, 21));
+
+        String fullName = a.getFullName();
+
+        Assert.assertNotEquals("PhilippTuor", fullName);
+    }
+
+    @Test
+    void getFullNameShouldNotReturnLastnameSpaceFirstname(){
+        Person a = new Person("Philipp","Tuor", LocalDate.of(1989, 06, 21));
+
+        String fullName = a.getFullName();
+
+        Assert.assertNotEquals("Tuor Philipp", fullName);
+    }
+
+
+
+    /***********************
+     * getAge
+     */
+
+    @Test
+    void getAgeShouldReturn10Years() throws Exception {
+        Person p = new Person("", "", LocalDate.now().minusYears(10));
+
+        Period actualAge = p.getAge();
+
+        Assert.assertEquals(10, actualAge.getYears());
     }
 
     @Test
     void getAgeReturns1DayIfYesterday() throws Exception {
         Person p = new Person("", "", LocalDate.now().minusDays(1));
 
-        // TODO implement
-        throw new NotImplementedException();
+        Period actualAge = p.getAge();
+
+        Assert.assertEquals(1, actualAge.getDays());
     }
-    // TODO some more useful tests
+
+    @Test
+    void getAgeReturns5MontschIfFiveMonthsAgo() throws Exception {
+        Person p = new Person("", "", LocalDate.now().minusMonths(5));
+
+        Period actualAge = p.getAge();
+
+        Assert.assertEquals(5, actualAge.getMonths());
+    }
+
 }
