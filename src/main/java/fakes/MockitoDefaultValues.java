608ea59e39853f42902996600a23ba59f6d67334
Ueli Kunz <kunz@ideadapt.net>
2019-11-06 21:16:27 +0100

    add mockito default values

---
+++ src/main/java/fakes/MockitoDefaultValues.java
@@ -0,0 +1,32 @@
+package fakes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockitoDefaultValues {
+
+  public void returnVoid(){
+  }
+
+  public boolean returnBoolean(){
+    return true;
+  }
+
+  public Integer returnInteger(){
+    return 10;
+  }
+
+  public int returnInt(){
+    return 10;
+  }
+
+  public User returnUser(){
+    return new User("");
+  }
+
+  public List<User> returnUserList(){
+    ArrayList userList = new ArrayList();
+    userList.add(new User(""));
+    return userList;
+  }
+}
