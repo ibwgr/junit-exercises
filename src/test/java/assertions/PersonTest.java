343ff8f6fff8c96f55793057d9dc2a680a9a2cf0
nadavisuals <44897257+nadavisuals@users.noreply.github.com>
2019-11-06 22:50:38 +0100

    work in progress

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -1,9 +1,10 @@
 package assertions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 class PersonTest {
 
@@ -11,29 +12,51 @@ class PersonTest {
 
     @Test
     void getFullNameReturnsFirstnameSpaceLastname(){
-        // TODO implement
-        throw new NotImplementedException();
+        Person p = new Person("James", "Bond", LocalDate.of(2019,11,05));
+        String fullname = p.getFullName();
+        Assertions.assertEquals("James Bond", fullname);
+
     }
 
     // TODO some more useful tests
 
-    // getAge
+    // getAge (Problem: Dieser Test funktioniert datumsabhängig, Morgen geht er nicht mehr. Leider noch keine funktionierende Lösung gefunden..)
+    @Test
+    void getAgeReturnsAmountOfDaysSinceBirthUntilToday(){
+         Person p = new Person("", "", LocalDate.of(2019, 11, 05));
+         Period age = p.getAge();
+
+         Assertions.assertEquals(0, age.getYears());
+         Assertions.assertEquals(0, age.getMonths());
+         Assertions.assertEquals(1, age.getDays());
+    }
 
 
     // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
     @Test
-    void getAgeReturns10YearsIfBornIn2009() throws Exception {
-        Person p = new Person("", "", LocalDate.of(2009, 1, 1));
+    void getAgeReturns20YearsIfBornIn1999() throws Exception {
+        Person p = new Person("", "", LocalDate.now().minusYears(20));
 
-        throw new NotImplementedException();
+        Period age = p.getAge();
+
+        Assertions.assertEquals(20, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(0, age.getDays());
+
     }
 
     @Test
     void getAgeReturns1DayIfYesterday() throws Exception {
         Person p = new Person("", "", LocalDate.now().minusDays(1));
+        Period age = p.getAge();
 
-        // TODO implement
-        throw new NotImplementedException();
+        Assertions.assertEquals(0, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(1, age.getDays());
+
     }
     // TODO some more useful tests
+
 }
+
+
