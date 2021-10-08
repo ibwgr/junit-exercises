b5466cc6b61f29adec8576407ff35de2a21aae0e
Claudia <clodos_m92@hotmail.com>
2021-10-08 19:47:26 +0200

    yeah :)

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -1,8 +1,10 @@
 package assertions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 class PersonTest {
 
@@ -11,7 +13,10 @@ class PersonTest {
     @Test
     void getFullNameReturnsFirstnameSpaceLastname(){
         // TODO implement
-        throw new IllegalArgumentException("you should implement code here");
+       // throw new IllegalArgumentException("you should implement code here");
+        Person pers1 = new Person("Tobias", "Meier", null);
+        Assertions.assertEquals("Tobias Meier",pers1.getFullName());
+
     }
 
     // TODO some more useful tests
@@ -20,11 +25,15 @@ class PersonTest {
     // --- getAge
 
     @Test
-    void getAgeReturns10YearsIfBornIn2009() throws Exception {
+    void getAgeReturns10YearsIfBornIn2011() throws Exception {
         // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
-        Person p = new Person("", "", LocalDate.of(2009, 1, 1));
+        Person p = new Person("", "", LocalDate.now().minusYears(10));
 
-        throw new IllegalArgumentException("you should implement code here");
+        //throw new IllegalArgumentException("you should implement code here");
+        Period age = p.getAge();
+        Assertions.assertEquals(10,age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(0, age.getDays());
     }
 
     @Test
@@ -32,7 +41,11 @@ class PersonTest {
         Person p = new Person("", "", LocalDate.now().minusDays(1));
 
         // TODO implement
-        throw new IllegalArgumentException("you should implement code here");
+        //throw new IllegalArgumentException("you should implement code here");
+        Period age = p.getAge();
+        Assertions.assertEquals(1,age.getDays());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(0, age.getYears());
     }
 
     // TODO some more useful tests
