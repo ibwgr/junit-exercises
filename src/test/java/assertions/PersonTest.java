e5ad21d4ac8e87a1718132b092ed65cbafd9cd0b
Cla Tschenett <cla@aimara.ch>
2021-10-30 14:35:03 +0200

    yea! :-)

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -1,42 +1,71 @@
 package assertions;
 
-import org.junit.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Period;
 
-/**
- * Created by ideadapt on 09.10.16.
- */
-public class PersonTest {
+class PersonTest {
 
-    // getFullName
+    // --- getFullName
 
     @Test
-    public void getFullNameReturnsFirstnameSpaceLastname(){
-        // TODO implement
-        throw new NotImplementedException();
+    void getFullNameReturnsFirstnameSpaceLastname() {
+        Person p = new Person("Pinco", "Pallino", null);
+
+        String fullname = p.getFullName();
+
+        Assertions.assertEquals("Pinco Pallino", fullname);
     }
 
     // TODO some more useful tests
 
-    // getAge
 
+    // --- getAge
 
-    // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
     @Test
-    public void getAgeReturns10YearsIf2006() throws Exception {
-        Person p = new Person("", "", LocalDate.of(2006, 1, 1));
+    void getAgeReturns10YearsIfBorn10YearsAgo() {
+        // TODO verbessern. Hinweis: Repeatable (wiederholbar) zu jeder Zeit.
+        // Test sollte mit NOW funktionieren
+        Person p = new Person("", "", LocalDate.now().minusYears(10));
 
-        throw new NotImplementedException();
+        Period age = p.getAge();
+
+        Assertions.assertEquals(10, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(0, age.getDays());
+
     }
 
     @Test
-    public void getAgeReturns1DayIfYesterday() throws Exception {
+    void getAgeReturns1DayIfYesterday() {
         Person p = new Person("", "", LocalDate.now().minusDays(1));
 
-        // TODO implement
-        throw new NotImplementedException();
+        Period age = p.getAge();
+
+        Assertions.assertEquals(0, age.getYears());
+        Assertions.assertEquals(0, age.getMonths());
+        Assertions.assertEquals(1, age.getDays());
     }
-    // TODO some more useful tests
+
+    @Test
+    void getAgeReturns5Years4Months3DaysIf5Years4Months3DaysAgo() {
+        Person p = new Person("", "", LocalDate.now().minusYears(5).minusMonths(4).minusDays(3));
+
+        Period age = p.getAge();
+
+        Assertions.assertEquals(5, age.getYears());
+        Assertions.assertEquals(4, age.getMonths());
+        Assertions.assertEquals(3, age.getDays());
+    }
+
+    @Test
+    void getAgeGreaterThan30(){
+        Person p = new Person("", "", LocalDate.now().minusYears(31));
+
+        Period age = p.getAge();
+
+        Assertions.assertTrue(age.getYears() > 30);
+    }
 }
