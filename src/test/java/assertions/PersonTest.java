08f7846593a4313383bd0f8747be52a01b4bbc32
Claudia <clodos_m92@hotmail.com>
2021-11-04 10:36:55 +0100

    isTeen Method in Person.java ergänzt PersonTest.java , Tests zu isTeen() geschrieben

--- src/test/java/assertions/PersonTest.java
+++ src/test/java/assertions/PersonTest.java
@@ -20,6 +20,42 @@ class PersonTest {
     }
 
     // TODO some more useful tests
+    @Test
+    void getFullNameCheckSpaceBetween(){
+        Person pers1 = new Person("Tobias", "Meier", null);
+        String fullName = pers1.getFullName();
+        Assertions.assertTrue(fullName.contains(" "), "True if space between firstname and lastname");
+    }
+
+    @Test
+    void isTeenAge18_12ReturnTrue(){
+        Person pers1 = new Person("", "", LocalDate.of(2003, 3,11));
+        Person pers2 = new Person("", "", LocalDate.of(2009, 3, 11));
+        Period age1 = pers1.getAge();
+        Period age2 = pers2.getAge();
+        int years1 = age1.getYears();
+        int years2 = age2.getYears();
+
+        Assertions.assertEquals(18, age1.getYears());
+        Assertions.assertEquals(12, age2.getYears());
+        Assertions.assertTrue(pers1.isTeen(years1),"Is teen = true");
+        Assertions.assertTrue(pers2.isTeen(years2),"Is teen = true");
+
+    }
+    @Test
+    void isNotTeenAge11_19ReturnFalse(){
+        Person pers1 = new Person("", "", LocalDate.of(2010, 3,11));
+        Person pers2 = new Person("", "", LocalDate.of(2002, 3, 11));
+        Period age1 = pers1.getAge();
+        Period age2 = pers2.getAge();
+        int years1 = age1.getYears();
+        int years2 = age2.getYears();
+
+        Assertions.assertEquals(11, age1.getYears());
+        Assertions.assertEquals(19, age2.getYears());
+        Assertions.assertFalse(pers1.isTeen(years1), "Not teen = false");
+        Assertions.assertFalse(pers2.isTeen(years2), "Not teen = false");
+    }
 
 
     // --- getAge
