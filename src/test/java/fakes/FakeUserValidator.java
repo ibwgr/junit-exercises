6ec7abf59f1c6170948ca3b953dc29d86a2ea7e0
Claudia <clodos_m92@hotmail.com>
2021-11-10 12:54:04 +0100

    FakeUserValidator verändert

--- src/test/java/fakes/FakeUserValidator.java
+++ src/test/java/fakes/FakeUserValidator.java
@@ -4,20 +4,31 @@ public class FakeUserValidator extends UserValidator{
 
 
     private  boolean doesUsernameExist;
+    private boolean isValidUsername;
 
     public FakeUserValidator(){
 
     }
 
-    public FakeUserValidator(boolean doesUserNameExist ) {
+    public FakeUserValidator(boolean doesUserNameExist, boolean isValidUsername ) {
         this.doesUsernameExist = doesUserNameExist;
+        this.isValidUsername = isValidUsername;
     }
 
-    @Override
-    public boolean doesUsernameExist(String username) {
+    /*@Override
+   public boolean doesUsernameExist(String username) {
         if(username.equals("kalua")){
             return true;
         }
         return false;
+    }*/
+    public boolean DoesUsernameExist(String username){
+        return doesUsernameExist;
     }
+
+    public boolean isValidUsername(String username){
+        return isValidUsername;
+    }
+
+
 }
