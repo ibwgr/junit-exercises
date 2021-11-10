aa6100444710f50ab9077259f2fe33595ae1e154
John <flnc85@hotmail.de>
2021-11-10 21:05:46 +0100

    Fake fertig, Mok und Mokito noch offen

---
+++ src/main/java/assertions/PMain.java
@@ -0,0 +1,30 @@
+package assertions;
+
+import java.time.LocalDate;
+
+/**
+ * @author - John Schmidt
+ * 08.11.2021, 00:40
+ */
+public class PMain {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Hans","Muster",null);
+        Person p2 = new Person("Frau","Musterfrau", LocalDate.of(2005,11,28));
+        Person p3 = new Person("Hans","Muster",LocalDate.now());
+        //Person p4 = new Person("Hans","Muster",(LocalDate.now()-(LocalDate.of(2021,10,14))));
+        Person p5 = new Person("Frau","Musterfrau", LocalDate.of(2021,10,14));
+        //getAgeReturns10YearsIfBornIn2009() //2021-10=2011
+        Person p6 = new Person("Maxüi","Maxümann", LocalDate.of(2011,10,15));
+
+        //System.out.println("p1: "+p1.getAge());
+        System.out.println("p2: "+p2.getAge());
+        System.out.println("p3: "+p3.getAge());
+
+        System.out.println("p5: "+p5.getAge());
+
+        System.out.println("p6 10: "+p6.getAge());
+        System.out.println("p6 name ü: "+p6.getFullName());
+
+    }
+}
